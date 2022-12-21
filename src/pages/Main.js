5ddc624eebdfc6deeb66254fc9ea117f78751af9
Roger Marques <marquesroger226@gmail.com>
2022-12-21 18:23:42 -0300

    [feat]: Botões de next e previous adicionados

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,12 +5,25 @@ import getData from '../helpers/getData';
 function Main() {
   const [games, setGames] = useState();
   const [loading, setIsLoading] = useState(true);
+  const [previous, setPrevious] = useState(null);
+  const [next, setNext] = useState(null);
+
+  const newGames = (endpoint) => {
+    setIsLoading(true);
+    getData(endpoint).then((gameData) => {
+      setGames(gameData.results);
+      setPrevious(gameData.previous);
+      setNext(gameData.next);
+    }).then(() => setIsLoading(false));
+  };
 
   useEffect(() => {
     const url = 'https://api.rawg.io/api/games?token&key=e338cac79cd8470c8b3c41797664aeb1';
     getData(url).then((gameData) => {
       setGames(gameData.results);
       setIsLoading(false);
+      setPrevious(gameData.previous);
+      setNext(gameData.next);
     });
   }, [setGames]);
 
@@ -20,22 +33,40 @@ function Main() {
     <div>
       <h1>Games</h1>
       <main>
-        {games?.map((game, index) => (
-          <Link to={ `/game/${game.id}` } key={ index }>
-            <div>
-              <img
-                src={ game.background_image }
-                alt={ game.name }
-                style={ { maxWidth: '200px' } }
-              />
-              <h2>{game.name}</h2>
-              <p>{game.rating}</p>
-              {game.genres.slice(0, 2).map((genre) => (
-                <p key={ genre.id }>{genre.name}</p>
-              ))}
-            </div>
-          </Link>
-        ))}
+        <div>
+          {games?.map((game, index) => (
+            <Link to={ `/game/${game.id}` } key={ index }>
+              <div>
+                <img
+                  src={ game.background_image }
+                  alt={ game.name }
+                  style={ { maxWidth: '200px' } }
+                />
+                <h2>{game.name}</h2>
+                <p>{game.rating}</p>
+                {game.genres.slice(0, 2).map((genre) => (
+                  <p key={ genre.id }>{genre.name}</p>
+                ))}
+              </div>
+            </Link>
+          ))}
+        </div>
+        <div>
+          <button
+            type="button"
+            disabled={ previous === null }
+            onClick={ () => newGames(previous) }
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            disabled={ next === null }
+            onClick={ () => newGames(next) }
+          >
+            Next
+          </button>
+        </div>
       </main>
     </div>
   );

04bbd25214acfec15618bec6560e7858669ff029
Roger Marques <marquesroger226@gmail.com>
2022-12-28 12:21:11 -0300

    [feat]: Renderiza conquistas na tela

--- src/pages/GameAchievements.js
+++ src/pages/GameAchievements.js
@@ -1,7 +1,67 @@
+import { useEffect, useState } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+import { useParams } from 'react-router-dom';
+import getData from '../helpers/getData';
+
 function GameAchievements() {
+  const [achievements, setAchievements] = useState([]);
+  const [isLoading, setIsLoading] = useState([false]);
+  const [previous, setPrevious] = useState(null);
+  const [next, setNext] = useState(null);
+
+  const { id } = useParams();
+
+  const newGames = (endpoint) => {
+    setIsLoading(true);
+    getData(endpoint).then((gameData) => {
+      setAchievements(gameData.results);
+      setPrevious(gameData.previous);
+      setNext(gameData.next);
+    }).then(() => setIsLoading(false));
+  };
+
+  useEffect(() => {
+    const url = `https://api.rawg.io/api/games/${id}/achievements?page_size=30&token&key=e338cac79cd8470c8b3c41797664aeb1`;
+    setIsLoading(true);
+    getData(url).then((data) => {
+      setAchievements(data.results);
+      setPrevious(data.previous);
+      setNext(data.next);
+      setIsLoading(false);
+    });
+  }, [id]);
+
+  if (isLoading) return <p>carregando...</p>;
+
   return (
     <div>
-      Achievements
+      <h1>Achievements</h1>
+      <ul>
+        {achievements.map((item) => (
+          <li key={ item.id }>
+            <img src={ item.image } alt={ item.name } />
+            <p>{item.name}</p>
+            <p>{item.description}</p>
+            <p>{`${item.percent}%`}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="buttons-container">
+        <button
+          type="button"
+          disabled={ previous === null }
+          onClick={ () => newGames(previous) }
+        >
+          <AiOutlineArrowLeft />
+        </button>
+        <button
+          type="button"
+          disabled={ next === null }
+          onClick={ () => newGames(next) }
+        >
+          <AiOutlineArrowRight />
+        </button>
+      </div>
     </div>
   );
 }

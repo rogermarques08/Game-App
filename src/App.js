9d4e5f35a253270aefe7a4506092c3a490bcc432
Roger Marques <marquesroger226@gmail.com>
2023-01-09 08:58:24 -0300

    [feat]: Implementa stylelint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 import GameAchievements from './pages/GameAchievements';
 import GameDetails from './pages/GameDetails';
 import GameList from './pages/GameList';
